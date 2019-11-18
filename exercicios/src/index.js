10040754fc8149e0758c7ea6b3c3c6a211e0691d
Enzo Glauber <enzojob@gmail.com>
2019-11-17 22:24:53 -0300

    Múltiplos  componentes

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import PrimeiroComponente from "./components/PrimeiroComponente";
+// import PrimeiroComponente from "./components/PrimeiroComponente";
+import { CompA, CompB as B } from "./components/DoisComponente";
 
 const elemento = document.getElementById('root')
 
 ReactDom.render(
   <div>
-    <PrimeiroComponente valor="Bom dia!"/>
+    <CompA valor="Olá eu sou A"></CompA>
+    <B valor="Olá eu sou B"></B>
+    {/* <PrimeiroComponente valor="Bom dia!"/> */}
   </div>
 , elemento)

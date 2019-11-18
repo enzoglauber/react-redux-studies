324bffbc56ee1b10dbb95f3b6685c187dec89d0f
Enzo Glauber <enzojob@gmail.com>
2019-11-17 22:10:15 -0300

    Arrow function

--- exercicios/src/components/PrimeiroComponente.jsx
+++ exercicios/src/components/PrimeiroComponente.jsx
@@ -1,7 +1,6 @@
-import { React } from "react"
+import React from "react"
 
-function PrimeiroComponente() {
-    return <h1>Primeiro Component!</h1>
-}
-
-export default PrimeiroComponente
+export default () =>
+    <div>
+        <h1>Primeiro Component! (Arrow) </h1>
+    </div>

60fef2707c648aaab8c4846d4bbbc30f2e697761
Enzo Glauber <enzojob@gmail.com>
2019-11-17 21:48:12 -0300

    JSX

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 const elemento = document.getElementById('root')
-ReactDom.render(<h1>Olá React</h1>, elemento)
+const jsx = <h1>Olá React</h1>
+ReactDom.render(jsx, elemento)

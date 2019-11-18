c2bf32ccc65b5e1cbac14c5f8cad5bf27b85adf4
Enzo Glauber <enzojob@gmail.com>
2019-11-17 22:32:53 -0300

    Multplos elementos (Frament, array, div)

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 // import PrimeiroComponente from "./components/PrimeiroComponente";
-import { CompA, CompB as B } from "./components/DoisComponente";
-
+// import { CompA, CompB as B } from "./components/DoisComponente";
+import MultiElementos from "./components/MultiElementos";
 const elemento = document.getElementById('root')
 
 ReactDom.render(
   <div>
-    <CompA valor="Olá eu sou A"></CompA>
+    <MultiElementos/>
+    {/* <CompA valor="Olá eu sou A"></CompA>
     <B valor="Olá eu sou B"></B>
-    {/* <PrimeiroComponente valor="Bom dia!"/> */}
+    <PrimeiroComponente valor="Bom dia!"/> */}
   </div>
 , elemento)

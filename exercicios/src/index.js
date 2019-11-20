b237ca229a9cb0683cb5f0d1b10bd91bd6488a37
Enzo Glauber <enzojob@gmail.com>
2019-11-19 21:09:55 -0300

    Children comunication

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import FamiliaSilva from './components/FamiliaSilva';
+// import FamiliaSilva from './components/FamiliaSilva';
+import Familia from './components/Familia';
+import Membro from './components/Membro';
 
 // import PrimeiroComponente from "./components/PrimeiroComponente";
 // import { CompA, CompB as B } from "./components/DoisComponente";
@@ -9,7 +11,15 @@ const elemento = document.getElementById('root')
 
 ReactDom.render(
   <div>
-    <FamiliaSilva/>
+    <Familia sobrenome="Machado">
+      <Membro nome="Enzo Glauber de Lima" ></Membro>
+      <Membro nome="Mary"></Membro>
+    </Familia>
+    
+    <Familia sobrenome="Legal">
+      <Membro nome="Jeol"></Membro>
+      <Membro nome="Carol"></Membro>
+    </Familia>
     {/* <CompA valor="Olá eu sou A"></CompA>
     <B valor="Olá eu sou B"></B>
     <PrimeiroComponente valor="Bom dia!"/> */}

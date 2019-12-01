6bfe043dc5efe70fec5bd2fdbff26214e2d90d42
Enzo Glauber <enzojob@gmail.com>
2019-12-01 16:58:49 -0300

    Cors

--- todo-app/back/src/api/todo/todo.js
+++ todo-app/back/src/api/todo/todo.js
@@ -1,10 +1,10 @@
-const restfull = require('node-restful')
-const mogoose = restfull.mogoose
+const restful = require('node-restful')
+const mongoose = restful.mongoose
 
-const todoSchema = new mogoose.Schema({
+const todoSchema = new mongoose.Schema({
     description: { type: String, required: true },
     done: { type: Boolean, required: true, default: false },
-    createdAt: { type: Date, default: Date.now}
+    createdAt: { type: Date, default: Date.now }
 })
 
-module.exports = restfull.Model('Todo', todoSchema)
+module.exports = restful.model('Todo', todoSchema)

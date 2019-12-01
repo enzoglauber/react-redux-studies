630d43f717e288a5f0aa143f4246e96c96277861
Enzo Glauber <enzojob@gmail.com>
2019-12-01 13:45:04 -0300

    ODM e API Rest

---
+++ todo-app/back/src/api/todo/todo.js
@@ -0,0 +1,10 @@
+const restfull = require('node-restful')
+const mogoose = restfull.mogoose
+
+const todoSchema = new mogoose.Schema({
+    description: { type: String, required: true },
+    done: { type: Boolean, required: true, default: false },
+    createdAt: { type: Date, default: Date.now}
+})
+
+module.exports = restfull.Model('Todo', todoSchema)

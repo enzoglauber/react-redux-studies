53690b1a8a33180bab43e47327c3234fed01bc4d
enzoglauber <enzojob@gmail.com>
2019-12-03 17:19:52 -0300

    integrando com o redux

--- todo-app/front/src/index.jsx
+++ todo-app/front/src/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './main/app'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const reducers = combineReducers({
+  field: () => ({ value: 'opa' })
+})
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+, document.getElementById('app'))

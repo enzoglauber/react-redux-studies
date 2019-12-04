610db594f1766eed31eb92f62da772e7939e9cc5
enzoglauber <enzojob@gmail.com>
2019-12-04 17:03:52 -0300

    config reducers

--- todo-app/front/src/index.jsx
+++ todo-app/front/src/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './main/app'
-import { combineReducers, createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-const reducers = combineReducers({
-  field: () => ({ value: 'opa' })
-})
+import App from './main/app';
+import reducers from './main/reducers';
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
 , document.getElementById('app'))

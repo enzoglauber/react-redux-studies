96cdb61d538dd33842cf431d4833f1fba3d61cce
Enzo Glauber <enzojob@gmail.com>
2019-12-09 19:01:50 -0300

    Checks and Radios

--- mui-theme/src/components/app/App.tsx
+++ mui-theme/src/components/app/App.tsx
@@ -5,14 +5,18 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 
 import { theme } from '../../shared/theme/main-theme';
-import Buttons from '../types/Buttons';
-import Types from '../types/Types';
+import Buttons from '../examples/Buttons';
+import Checks from '../examples/Checks';
+import Types from '../examples/Types';
+import Radios from '../examples/Radios';
 
 export default () => (
   <ThemeProvider theme={theme}>
     <Container>
       <Types />
       <Buttons />
+      <Checks />
+      <Radios />
     </Container>
   </ThemeProvider>
 )
--- mui-theme/src/shared/theme/main-theme.tsx
+++ mui-theme/src/shared/theme/main-theme.tsx
@@ -2,19 +2,14 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 export const theme = createMuiTheme({
   palette: {
-    primary: {
-      // light: will be calculated from palette.primary.main,
-      main: '#ffcc00',
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
+    primary: {      
+      main: '#ffff00',
     },
     secondary: {
       light: '#0066ff',
       main: '#0044ff',
-      // dark: will be calculated from palette.secondary.main,
       contrastText: '#ffcc00',
-    },
-    // error: will use the default color
-  },
+    }
+  }
 })
 
